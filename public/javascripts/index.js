6259d4525936dc8f1bcd212c498fdfcc48e866e8
garywei944 <33930674+garywei944@users.noreply.github.com>
2021-08-07 09:54:06 -0400

    Merge with lvd's code

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -10,28 +10,115 @@
 //     console.log("Closed", event);
 // };
 
-function initClientBtn(i) {
-    let $clientBtn = $("#clientBtn" + i);
-    $clientBtn.on("click", function () {
-        if ($clientBtn.text() === "Connected") {
-            $clientBtn.removeClass("btn-outline-success");
-            $clientBtn.addClass("btn-outline-danger");
-            $clientBtn.html("Disconnected");
+// Change log
+// 1. Use let instead of var
+// 2. Use === instead of ==
+
+let currentContent = "";
+const data = {
+    date: 0,
+    content: "data",
+}
+let database = [];
+const client = {
+    state: 1,
+    updatedDate: 0,
+    updatedIndex: 0,
+}
+let clients = [];
+let clientUIBlock;
+
+function addClient() {
+    clients[clients.length] = JSON.parse(JSON.stringify(client));
+    let newBlock = clientUIBlock.innerHTML.replace(/XXX/g, String(clients.length));
+    $("#clientList").append(newBlock);
+    let $simulate = $("#simulate" + clients.length);
+    $simulate.on("click", function () {
+        let index = Number($(this).attr("id").substring(8));
+        if ($simulate.text() === "Reconnect") {
+            $simulate.removeClass("btn-outline-success");
+            $simulate.addClass("btn-outline-danger");
+            $simulate.html("Disconnect");
+            clients[index - 1].state = 1;
         } else {
-            $clientBtn.removeClass("btn-outline-danger");
-            $clientBtn.addClass("btn-outline-success");
-            $clientBtn.html("Connected");
+            $simulate.removeClass("btn-outline-danger");
+            $simulate.addClass("btn-outline-success");
+            $simulate.html("Reconnect");
+            clients[index - 1].state = 0;
         }
     })
 }
 
 $(function () {
-    console.log('Success');
-
-    for (let i = 1; i <= 2; i++) {
-        initClientBtn(i);
-    }
-
-    $("clientForm2").clone()
-
+    $("#addClient").on("click", function () {
+        addClient();
+    })
+    $("#restart").on("click", function () {
+        currentContent = "";
+        $("#hostText").val("");
+        database.splice(0, database.length);
+        for (let i = 0; i < clients.length; i = i + 1) {
+            document.getElementById("client" + (i + 1)).remove()
+        }
+        clients.splice(0, clients.length);
+    })
+    clientUIBlock = document.getElementById("clientForm").cloneNode(true);
+    document.getElementById("clientForm").remove()
+    document.getElementById("clientList").removeAttribute("hidden");
+    $("#hostText").on("input", function () {
+        let value = $(this).val();
+        let condition = 0;
+        if (value.substring(currentContent.length).length > 1) {
+            condition = 1;
+        } else if (value.substring(0, currentContent.length) != currentContent) {
+            condition = 1;
+        }
+        if (condition) {
+            $(this).val(currentContent);
+        } else {
+            let character = value.substring(currentContent.length);
+            const newData = JSON.parse(JSON.stringify(data));
+            newData.date = Date.now();
+            newData.content = character;
+            database[database.length] = newData;
+            currentContent = value;
+        }
+        this.setSelectionRange(currentContent.length, currentContent.length);
+    })
+    update();
 });
+
+function update() {
+    window.setTimeout(update, 50);
+    if (database.length === 0) {
+        return
+    }
+    for (let i = 0; i < clients.length; i = i + 1) {
+        let $text = $("#clientText" + (i + 1));
+        if (clients[i].state > 0) {
+            if (clients[i].state === 2) {
+                $("#clientLabel" + (i + 1)).html('Client ' + (i + 1) + ': <font color="green">Connected and synchronized.</font>');
+            } else if (clients[i].state === 1) {
+                $("#clientLabel" + (i + 1)).html('Client ' + (i + 1) + ': <font color="orange">Connected. Catching up...</font>');
+            }
+            if (clients[i].updatedDate < database[database.length - 1].date) {
+                for (let j = clients[i].updatedIndex; j < database.length; j = j + 1) {
+                    if (database[j].date > clients[i].updatedDate) {
+                        $text.text($text.val() + database[j].content);
+                        $text.scrollTop($text[0].scrollHeight);
+                        clients[i].updatedDate = database[j].date;
+                        clients[i].updatedIndex = j;
+                        if (clients[i].state === 1) {
+                            if (j === (database.length - 1)) {
+                                clients[i].state = 2;
+                            }
+                            break
+                        }
+                    }
+                }
+            }
+        } else {
+            $("#clientLabel" + (i + 1)).html('Client ' + (i + 1) + ': <font color="red">Disconnected.</font>');
+        }
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -11,39 +11,37 @@ function loadMessage(conn, str) {
 /* GET home page. */
 router.get('/', function (req, res, next) {
 
-    let port = 0
-    let result = {}
+    // let port = 0
+    // let result = {}
+    //
+    // let srv = net.createServer(function (sock) {
+    //     sock.end('Hello from server\n');
+    // });
+    // srv.listen(0, function () {
+    //     port = srv.address().port;
+    //     console.log('DEMO: Create port on '+port)
+    // });
+    //
+    // console.log(port)
+    //
+    // // Create websocket
+    // let server = ws
+    //     .createServer(function (conn) {
+    //         // 监听消息
+    //         conn.on("text", function (str) {
+    //             // 调用相关方法
+    //             loadMessage(conn, str);
+    //         });
+    //         // Connection closed
+    //         conn.on("close", function (code, reason) {
+    //             console.log("Connection closed.");
+    //         });
+    //     })
+    //     .listen(port);
+    //
+    // console.log("Connection created.");
 
-    let srv = net.createServer(function (sock) {
-        sock.end('Hello from server\n');
-    });
-    srv.listen(0, function () {
-        port = srv.address().port;
-        console.log('DEMO: Create port on '+port)
-    });
-
-    console.log(port)
-
-    // Create websocket
-    let server = ws
-        .createServer(function (conn) {
-            // 监听消息
-            conn.on("text", function (str) {
-                // 调用相关方法
-                loadMessage(conn, str);
-            });
-            // Connection closed
-            conn.on("close", function (code, reason) {
-                console.log("Connection closed.");
-            });
-        })
-        .listen(port);
-
-    console.log("Connection created.");
-
-    res.render('index', {
-        "port": port
-    });
+    res.render('index');
 });
 
 module.exports = router;
